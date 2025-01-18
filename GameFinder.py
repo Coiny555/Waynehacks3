e531ff41b7e85632828a3a3686c9f9ed6decae18
Kenyon <hg7959@wayne.edu>
2025-01-18 15:06:17 -0500

    Adding pages to the webapp!

--- GameFinder.py
+++ GameFinder.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #For the pages on the app 
 def home():
-    return "Hello, Flask! Goodbye boi!"
+    return render_template('home.html')
+
+@app.route('/about') #For the about page
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
